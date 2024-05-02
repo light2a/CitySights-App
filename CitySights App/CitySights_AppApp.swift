abf7ab47682d6ae466485b97b6273bc0915a88e1
duc <leminhduc2834@gmail.com>
2024-05-02 21:48:03 +0700

    Initial Commit

---
+++ CitySights App/CitySights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySights_AppApp.swift
+//  CitySights App
+//
+//  Created by duc on 02/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct CitySights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
