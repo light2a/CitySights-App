e2716140f307deaf1d8645289f506108b5360756
duc <leminhduc2834@gmail.com>
2024-05-04 09:21:47 +0700

    - add dataservice and business search method api call - fire off api call when main view appears

--- CitySights App/ContentView.swift
+++ CitySights App/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     @State var query: String = ""
+    var service = DataService()
     
     var body: some View {
         HStack {
@@ -26,9 +27,10 @@ struct ContentView: View {
             }
         }
         .padding()
-        .onAppear() {
-            print(Bundle.main.infoDictionary?["API_KEY"] as? String)
+        .task {
+            await service.businessSearch()
         }
+        
     }
 }
 
---
+++ CitySights App/DataService.swift
@@ -0,0 +1,37 @@
+//
+//  DataService.swift
+//  CitySights App
+//
+//  Created by duc on 03/05/2024.
+//
+
+import Foundation
+
+struct DataService {
+    
+    let apikey = Bundle.main.infoDictionary?["API_KEY"] as? String
+    
+    func businessSearch() async {
+        //check api key exist or not
+        guard apikey != nil else {
+            return
+        }
+        //1 .  create url
+        if let url = URL(string: "https://api.yelp.com/v3/businesses/search?latitude=35.665517&longitude=139.770398&categories=restaurants&limit=10"){
+            //2.  create request
+            var request = URLRequest(url: url)
+            request.addValue("Bearer \(apikey!)", forHTTPHeaderField: "Authorization")
+            request.addValue("application/json", forHTTPHeaderField: "accept")
+
+            //3. Send request
+            do {
+                let (data, response) = try await URLSession.shared.data(for: request)
+                print(data)
+                print(response)
+            }
+            catch{
+                print(error)
+            }
+        }
+    }
+}
