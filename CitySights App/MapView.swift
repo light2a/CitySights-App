9f79264927ff56b5b2f117d01be13acb58866781
duc <leminhduc2834@gmail.com>
2024-05-07 07:55:16 +0700

    - add map marker to repeate business - detech selection  business in map

--- CitySights App/MapView.swift
+++ CitySights App/MapView.swift
@@ -9,11 +9,29 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    @Environment(BusinessModel.self) private var model
+    @State var selectedId: String?
     var body: some View {
-        Map()
+        Map(selection: $selectedId) {
+            ForEach(model.businesses, id: \.id){ b in
+                Marker(b.name ?? "Res", coordinate: CLLocationCoordinate2D(latitude: b.coordinates?.latitude ?? 0, longitude: b.coordinates?.longitude ?? 0))// toa do
+                    .tag(b.id ?? "")// optional thi lo ma doi
+            }
+        }
+        .onChange(of: selectedId) { oldValue, newValue in
+            //find the business which matches this id
+            let business = model.businesses.first { bu in
+                bu.id == selectedId
+                
+            }
+            // if the business is found, set it as the selected one
+            if business != nil {
+                model.selectedBusiness = business!
+            }
+        }
     }
 }
 
 #Preview {
-    MapView()
+    MapView().environment(BusinessModel())
 }
